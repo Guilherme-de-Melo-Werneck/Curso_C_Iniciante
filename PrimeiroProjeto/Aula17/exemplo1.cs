ae1a74d82882ffa9d14a0476fed3f9fb5db577af
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-13 22:24:15 -0300

    Curso

--- PrimeiroProjeto/Aula17/exemplo1.cs
+++ PrimeiroProjeto/Aula17/exemplo1.cs
@@ -12,8 +12,27 @@ namespace PrimeiroProjeto.Aula17
         {
 
             string frase = Console.ReadLine();
+            string x  = Console.ReadLine();
+            string y =  Console.ReadLine();
+            string z = Console.ReadLine();
 
+            string s = Console.ReadLine();
+
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+            Console.WriteLine("Você digitou: ");
             Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(s);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
 
 
         }
