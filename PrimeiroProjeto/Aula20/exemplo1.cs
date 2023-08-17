b919869124389b5b3c7bd359796fc0be466f38cd
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-17 19:09:05 -0300

    Curso

---
+++ PrimeiroProjeto/Aula20/exemplo1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Aula20
+{
+    internal class exemplo1
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, valormquadrado, area, valor;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valormquadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = largura * comprimento;
+            valor = area * valormquadrado;
+
+            Console.WriteLine($"A largura do retangulo é: " + largura.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"O comprimento do retangulo é: " + comprimento.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"O valor do metro quadrado é: " + valormquadrado.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"O valor da area é: " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"O valor total do terreno é: " + valor.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
