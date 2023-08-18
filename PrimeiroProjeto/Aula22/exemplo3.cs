42bd0e6ccd36a4b9b9cc643a31491ee19da0f5b4
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-18 19:10:15 -0300

    Curso

---
+++ PrimeiroProjeto/Aula22/exemplo3.cs
@@ -0,0 +1,35 @@
+﻿//Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável P
+//ROD. A seguir mostre a variável PROD com mensagem correspondente.   
+
+//Entrada
+//O arquivo de entrada contém 2 valores inteiros.
+
+//Saída
+//Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
+//Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Aula22
+{
+    internal class exemplo3
+    {
+        static void Main(string[] args)
+        {
+            int A, B, PROD;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            PROD = A * B;
+
+            Console.WriteLine("PROD = " + PROD);
+
+
+        }
+    }
+}
