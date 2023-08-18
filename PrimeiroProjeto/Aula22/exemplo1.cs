ee946ed249e8e89687c73c7bb8e66c49b9697fa1
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-18 17:55:17 -0300

    Curso

---
+++ PrimeiroProjeto/Aula22/exemplo1.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Globalization;
+
+//namespace PrimeiroProjeto.Aula22
+//{
+//    internal class exemplo1
+//    {
+//        static void Main(string[] args)
+//        {
+//            double area, n, raio;
+//            n = 3.14159;
+//            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//            area = n * Math.Pow(raio, 2);
+
+//            Console.WriteLine("A = " + area.ToString("F4"));
+//        }
+//    }
+//}
---
+++ PrimeiroProjeto/Aula22/exemplo2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Aula22
+{
+    internal class exemplo2
+    {
+        static void Main(string[] args)
+        {
+            int A, B, SOMA;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            SOMA = A + B;
+
+            Console.WriteLine("SOMA = " + SOMA);
+        }
+    }
+}
