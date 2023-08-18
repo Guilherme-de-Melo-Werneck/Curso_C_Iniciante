eaa4c9bf6d673f24a5415319dbdd88672a5d0512
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-18 19:16:37 -0300

    Curso

--- PrimeiroProjeto/Aula22/exemplo3.cs
+++ PrimeiroProjeto/Aula22/exemplo3.cs
@@ -8,28 +8,28 @@
 //Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. 
 //Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace PrimeiroProjeto.Aula22
-{
-    internal class exemplo3
-    {
-        static void Main(string[] args)
-        {
-            int A, B, PROD;
+//namespace PrimeiroProjeto.Aula22
+//{
+//    internal class exemplo3
+//    {
+//        static void Main(string[] args)
+//        {
+//            int A, B, PROD;
 
-            A = int.Parse(Console.ReadLine());
-            B = int.Parse(Console.ReadLine());
+//            A = int.Parse(Console.ReadLine());
+//            B = int.Parse(Console.ReadLine());
 
-            PROD = A * B;
+//            PROD = A * B;
 
-            Console.WriteLine("PROD = " + PROD);
+//            Console.WriteLine("PROD = " + PROD);
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ PrimeiroProjeto/Aula22/exemplo4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Aula22
+{
+    internal class exemplo4
+    {
+    }
+}
