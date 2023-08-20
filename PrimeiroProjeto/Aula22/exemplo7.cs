5bf5faf91bad442d3b64c01c4a58af8d14fe0baf
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-20 00:25:10 -0300

    Curso

--- PrimeiroProjeto/Aula22/exemplo7.cs
+++ PrimeiroProjeto/Aula22/exemplo7.cs
@@ -1,27 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+﻿//Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o 
+//salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
 
-namespace PrimeiroProjeto.Aula22
-{
-    internal class exemplo7
-    {
-        static void Main(string[] args)
-        {
-            int numero, horas;
-            double vhora, salario;
+//Entrada
+//O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas 
+//trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
 
-            numero = int.Parse(Console.ReadLine());
-            horas = int.Parse(Console.ReadLine());
-            vhora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//Saída
+//Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. 
+//No caso do salário, também deve haver um espaço em branco após o $.
 
-            salario = vhora * horas;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Globalization;
 
-            Console.WriteLine("NUMBER = " + numero);
-            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
-        }
-    }
-}
+//namespace PrimeiroProjeto.Aula22
+//{
+//    internal class exemplo7
+//    {
+//        static void Main(string[] args)
+//        {
+//            int numero, horas;
+//            double vhora, salario;
+
+//            numero = int.Parse(Console.ReadLine());
+//            horas = int.Parse(Console.ReadLine());
+//            vhora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//            salario = vhora * horas;
+
+//            Console.WriteLine("NUMBER = " + numero);
+//            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+//        }
+//    }
+//}
---
+++ PrimeiroProjeto/Aula22/exemplo8.cs
@@ -0,0 +1,40 @@
+﻿//Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). 
+//Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
+
+//Entrada
+//O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double) com duas casas decimais, 
+//representando o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
+
+//Saída
+//Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Globalization;
+
+//namespace PrimeiroProjeto.Aula22
+//{
+//    internal class exemplo8
+//    {
+//        static void Main(string[] args)
+//        {
+//            string nome;
+//            double salariofixo, totalvenda, salariototal;
+
+//            nome = Console.ReadLine();
+//            salariofixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//            totalvenda = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture );
+
+//            salariototal = totalvenda * 15 / 100 + salariofixo;
+
+//            Console.WriteLine();
+
+//            Console.WriteLine("TOTAL = R$ " + salariototal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+//        }
+//    }
+//}
