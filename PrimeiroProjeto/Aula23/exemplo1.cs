faef1bce358a4f1afacf428190786c71cd08491d
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-21 17:55:08 -0300

    Curso

---
+++ PrimeiroProjeto/Aula23/exemplo1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace PrimeiroProjeto.Aula23
+{
+    internal class exemplo1
+    {
+        static void Main(string[] args)
+        {
+            double a, b, area, perimetro, diagonal;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = a * b;
+
+            perimetro = (2 * b) + (2 * a);
+
+            diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
