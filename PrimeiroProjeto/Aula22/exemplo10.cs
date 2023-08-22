4820a93f8be6ffd406f55279956b960c5daa3c80
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-22 15:16:24 -0300

    Curso

---
+++ PrimeiroProjeto/Aula22/exemplo10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace PrimeiroProjeto.Aula22
+{
+    internal class exemplo10
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double y, kml;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            kml = (double) x / y;
+
+            Console.WriteLine($"{kml.ToString("f3", CultureInfo.InvariantCulture)} km/l");
+        }
+    }
+}
