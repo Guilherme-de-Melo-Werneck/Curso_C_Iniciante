34eab5baf95cbd97953c67d9e1649c4342e57a15
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-08-22 14:51:48 -0300

    Curso

--- PrimeiroProjeto/Aula22/exemplo9.cs
+++ PrimeiroProjeto/Aula22/exemplo9.cs
@@ -1,4 +1,4 @@
-﻿//este problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, 
+﻿//este problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2,
 //o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
 
 //Entrada
@@ -14,6 +14,8 @@
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
+//using System.Globalization;
+//using System.Net;
 
 //namespace PrimeiroProjeto.Aula22
 //{
@@ -21,7 +23,25 @@
 //    {
 //        static void Main(string[] args)
 //        {
+//            int cod1, cod2, qtd1, qtd2;
+//            double preco1, preco2, valortotal;
+//            string[] vet;
 
+//            vet = Console.ReadLine().Split(' ');
+//            cod1 = int.Parse(vet[0]);
+//            qtd1 = int.Parse(vet[1]);
+//            preco1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+//            vet = Console.ReadLine().Split(' ');
+//            cod2 = int.Parse(vet[0]);
+//            qtd2 = int.Parse(vet[1]);
+//            preco2 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+//            valortotal = preco1 * qtd1 + preco2 * qtd2;
+
+//            Console.WriteLine($"VALOR A PAGAR: R$ {valortotal.ToString("F2", CultureInfo.InvariantCulture)}");
 //        }
 //    }
 //}
+
+
