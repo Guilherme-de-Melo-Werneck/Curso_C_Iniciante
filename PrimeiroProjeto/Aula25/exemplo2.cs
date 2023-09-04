c3f6a740d7540fc676c6ee7d8b2727ddc5f60b9e
Guilherme-de-Melo-Werneck <guilhermemwerneck@hotmail.com>
2023-09-04 00:48:01 -0300

    Curso

--- PrimeiroProjeto/Aula25/exemplo2.cs
+++ PrimeiroProjeto/Aula25/exemplo2.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Globalization;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Globalization;
 
-namespace PrimeiroProjeto.Aula25
-{
-    internal class exemplo2
-    {
-        static void Main(string[] args)
-        {
-            double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
-            string[] valores = Console.ReadLine().Split(' ');
-            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
-            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
-            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+//namespace PrimeiroProjeto.Aula25
+//{
+//    internal class exemplo2
+//    {
+//        static void Main(string[] args)
+//        {
+//            double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+//            string[] valores = Console.ReadLine().Split(' ');
+//            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+//            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+//            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
 
-            triangulo = A * C / 2;
-            circulo = Math.PI * C * C;
-            trapezio = (A + B) * C / 2;
-            quadrado = B * B;
-            retangulo = A * B;
+//            triangulo = A * C / 2;
+//            circulo = Math.PI * C * C;
+//            trapezio = (A + B) * C / 2;
+//            quadrado = B * B;
+//            retangulo = A * B;
 
-            Console.WriteLine("TRIANGULO = " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("CIRCULO = " + circulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("TRAPEZIO = " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("QUADRADO = " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("RETANGULO = " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+//            Console.WriteLine("TRIANGULO = " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+//            Console.WriteLine("CIRCULO = " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+//            Console.WriteLine("TRAPEZIO = " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+//            Console.WriteLine("QUADRADO = " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+//            Console.WriteLine("RETANGULO = " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ PrimeiroProjeto/Aula25/exemplo3.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace PrimeiroProjeto.Aula25
+//{
+//    internal class exemplo3
+//    {
+//        static void Main(string[] args) 
+//        {
+//            int x, y, z, a, b;
+//            string[] valores = Console.ReadLine().Split(' ');
+//            x = int.Parse(valores[0]);
+//            y = int.Parse(valores[1]);
+//            z = int.Parse(valores[2]);
+
+//            a = (x + y + Math.Abs(x - y)) /2;
+//            b = (a + z + Math.Abs(a - z)) /2;
+
+//            Console.WriteLine($"{b} eh o maior");
+
+
+               
+        
+//        }
+//    }
+//}
